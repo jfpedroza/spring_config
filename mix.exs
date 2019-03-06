7bee153bf29f49bae9ed6ac88adcfef45ead91a5
Jhon Pedroza <jhonfpedroza@gmail.com>
2019-03-05 23:38:21 -0500

    Configure ExDoc and Hex package

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule SpringConfig.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :spring_config,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: @version,
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      name: "SpringConfig",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      docs: docs(),
+      source_url: "https://github.com/johnf9896/spring_config"
     ]
   end
 
@@ -21,6 +29,28 @@ defmodule SpringConfig.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Consume configuration from a Spring Cloud Config Server in Elixir."
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      maintainers: ["Jhon Pedroza"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/johnf9896/spring_config"}
+    ]
   end
 end
